a344b4e4e17e24d576521cc5969bb038709c9281
Phat Tran <ptran@strongtie.com>
2024-01-08 07:32:40 +0700

    Inherit the WeaponDamage class

---
+++ ArrowDamage/SwordDamage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrowDamage
+{
+    class SwordDamage : WeaponDamage
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        /// <summary>
+        /// Calculates the damage based on the current properties.
+        /// </summary>
+        protected override void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+
+        /// <summary>
+        /// The constructor calculates damage based on default Magic
+        /// and Flaming values and a starting 3d6 roll.
+        /// </summary>
+        /// <param name="startingRoll">Starting 3d6 roll</param>
+        public SwordDamage(int startingRoll) : base(startingRoll) { }
+    }
+}
---
+++ ArrowDamage/WeaponDamage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrowDamage
+{
+    class WeaponDamage
+    {
+        /// <summary>
+        /// Contains the calculated damage.
+        /// </summary>
+        public int Damage { get; protected set; }
+        private int roll;
+        /// <summary>
+        /// Sets or gets the 3d6 roll.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        private bool magic;
+        /// <summary>
+        /// True if the sword is magic, false otherwise.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        private bool flaming;
+        /// <summary>
+        /// True if the sword is flaming, false otherwise.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Calculates the damage based on the current properties.
+        /// </summary>
+        protected virtual void CalculateDamage() { /* the subclass overrides this */ }
+
+        public WeaponDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
